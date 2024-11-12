a9d5918e4434d54d1641b65e74daef90fae7d49b
Carlos Barboza <barbozacarlos678@gmail.com>
2024-11-12 20:23:46 -0300

    Add sections and sections components.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'background-light': 'rgb(30 64 175)',
+        'background-dark': '#222',
+      },
+    },
+  },
+  plugins: [],
 }
