3c1dac39b1cbc2ff577833b1465dc19f8faa6f98
Carlos Barboza <barbozacarlos678@gmail.com>
2024-11-28 23:28:06 -0300

    Added a profile image to the introduction section, added rotation to the technology icons, improved the shadow that follows the mouse.

--- src/animations/animations.ts
+++ src/animations/animations.ts
@@ -16,17 +16,20 @@ export const observeCards = (): void => {
 
 export const mouseTracker = (): void => {
   const shadow = document.createElement('div')
-  shadow.style.position = 'fixed'
-  shadow.style.width = '50px'
-  shadow.style.height = '50px'
+  shadow.classList.add(
+    'fixed',
+    'w-[30px]',
+    'h-[30px]',
+    'rounded-full',
+    'z-[1]',
+    'top-0',
+    'left-0'
+  )
+
   shadow.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
-  shadow.style.boxShadow = shadow.style.borderRadius = '50%'
   shadow.style.pointerEvents = 'none'
-  shadow.style.zIndex = '1000'
-  shadow.style.top = '0'
-  shadow.style.left = '0'
   shadow.style.transform = 'translate(0, 0)'
-  shadow.style.transition = 'transform 0.1s ease'
+  shadow.style.transition = 'transform .1s ease'
   shadow.style.boxShadow = '0px 4px 20px rgba(0, 0, 0, 1)'
   document.body.appendChild(shadow)
 
